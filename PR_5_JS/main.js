0c511a17cb0729d319375b038336bf80bcf3fb01
harazduyk <tatuyana.harazduyk@gmail.com>
2022-11-17 19:44:37 +0200

    pr5_js

---
+++ PR_5_JS/main.js
@@ -0,0 +1,158 @@
+window.addEventListener('load', () => {
+	todos = JSON.parse(localStorage.getItem('todos')) || [];
+	const nameInput = document.querySelector('#name');
+	const newTodoForm = document.querySelector('#new-todo-form');
+	const hourEl = document.getElementById('hour')
+	const minuteEl = document.getElementById('minute')
+	const secondEl = document.getElementById('second')
+	const dayEl = document.getElementById('day')
+	const monthEl = document.getElementById('month')
+	const yearEl = document.getElementById('year')
+
+
+	function getTime() {
+		const now = new Date()
+		const date = now.getDate() < 10 ? '0' + now.getDate() : now.getDate()
+		const month = now.getMonth() < 10 ? '0' + (now.getMonth() + 1) : now.getMonth()
+		const year = now.getFullYear()
+
+		const hour = now.getHours() < 10 ? '0' + now.getHours() : now.getHours()
+		const minute =
+			now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes()
+		const second =
+			now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds()
+
+		const months = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December',
+		]
+
+		const month_title = now.getMonth();
+
+		dayEl.textContent = date
+		monthEl.textContent = months[month_title]
+		yearEl.textContent = year
+		hourEl.textContent = hour
+		minuteEl.textContent = minute
+		secondEl.textContent = second
+
+		return `${hour}:${minute}, ${date}. ${month}. ${year}`
+	}
+	setInterval(getTime, 1000)
+
+	newTodoForm.addEventListener('submit', e => {
+		e.preventDefault();
+
+		const todo = {
+			content: e.target.elements.content.value,
+			category: e.target.elements.category.value,
+			done: false,
+			createdAt: new Date().getTime()
+		}
+
+		todos.push(todo);
+
+		localStorage.setItem('todos', JSON.stringify(todos));
+
+		// Reset the form
+		e.target.reset();
+
+		DisplayTodos()
+	})
+
+	DisplayTodos()
+})
+
+function DisplayTodos () {
+	const todoList = document.querySelector('#todo-list');
+	todoList.innerHTML = "";
+
+	todos.forEach(todo => {
+		const todoItem = document.createElement('div');
+		todoItem.classList.add('todo-item');
+
+		const label = document.createElement('label');
+		const input = document.createElement('input');
+		const span = document.createElement('span');
+		const content = document.createElement('div');
+		const actions = document.createElement('div');
+		const edit = document.createElement('button');
+		const deleteButton = document.createElement('button');
+
+		input.type = 'checkbox';
+		input.checked = todo.done;
+		span.classList.add('bubble');
+		if (todo.category == 'personal') {
+			span.classList.add('personal');
+		} else {
+			span.classList.add('business');
+		}
+		content.classList.add('todo-content');
+		actions.classList.add('actions');
+		edit.classList.add('edit');
+		deleteButton.classList.add('delete');
+
+		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+		edit.innerHTML = 'Edit';
+		deleteButton.innerHTML = 'Delete';
+
+		label.appendChild(input);
+		label.appendChild(span);
+		actions.appendChild(edit);
+		actions.appendChild(deleteButton);
+		todoItem.appendChild(label);
+		todoItem.appendChild(content);
+		todoItem.appendChild(actions);
+
+		todoList.appendChild(todoItem);
+
+		if (todo.done) {
+			todoItem.classList.add('done');
+		}
+		
+		input.addEventListener('change', (e) => {
+			todo.done = e.target.checked;
+			localStorage.setItem('todos', JSON.stringify(todos));
+
+			if (todo.done) {
+				todoItem.classList.add('done');
+			} else {
+				todoItem.classList.remove('done');
+			}
+
+			DisplayTodos()
+
+		})
+
+		edit.addEventListener('click', (e) => {
+			const input = content.querySelector('input');
+			input.removeAttribute('readonly');
+			input.focus();
+			input.addEventListener('blur', (e) => {
+				input.setAttribute('readonly', true);
+				todo.content = e.target.value;
+				localStorage.setItem('todos', JSON.stringify(todos));
+				DisplayTodos()
+
+			})
+		})
+
+		deleteButton.addEventListener('click', (e) => {
+			todos = todos.filter(t => t != todo);
+			localStorage.setItem('todos', JSON.stringify(todos));
+			DisplayTodos()
+		})
+
+	})
+}
+
