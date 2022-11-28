414e7091746a3e31cf9af0f27043af29660e396d
harazduyk <tatuyana.harazduyk@gmail.com>
2022-11-28 10:54:35 +0200

    lr10_js

---
+++ LR_10_JS/js/calcCartPrice.js
@@ -0,0 +1,26 @@
+function calcCartPriceAndDelivery() {
+	const cartWrapper = document.querySelector('.cart-wrapper');
+	const priceElements = cartWrapper.querySelectorAll('.price__currency');
+	const totalPriceEl = document.querySelector('.total-price');
+	const deliveryCost = document.querySelector('.delivery-cost');
+	const cartDelivery = document.querySelector('[data-cart-delivery]');
+
+	// загальна сума товарів
+	let priceTotal = 0;
+
+	priceElements.forEach(function (item) {
+		const amountEl = item.closest('.cart-item').querySelector('[data-counter]');
+		priceTotal += parseInt(item.innerText) * parseInt(amountEl.innerText);
+	});
+
+	totalPriceEl.innerText = priceTotal;
+
+	if (priceTotal > 0) {
+		cartDelivery.classList.remove('none');
+	} else {
+		cartDelivery.classList.add('none');
+	}
+
+	// Указываем стоимость доставки
+
+}
